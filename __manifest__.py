22b197a6c6b19a3df83a29888a7f0e12671dad16
FWA-Khoerul.MUTAQIN <khoerul.mutaqin@falinwa.com>
2025-10-28 14:18:21 +0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "x_warning_existing_name",
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'description': """Long description of module's purpose""",
+    "version": "18.0.0.0.0",    
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'depends': ['base','product','base_automation'],
+    'data': [
+        'data/ir_actions_server.xml',
+        'data/base_automation.xml',
+    ],
+}
+
